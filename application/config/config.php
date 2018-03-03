067176283f2191fb11dba6491064f2ca66f2714f
agungdh <agungdh@live.com>
2018-03-04 00:31:34 +0700

    ganti port

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost:81/limbahb3';
+$config['base_url'] = 'http://localhost/limbahb3';
 
 /*
 |--------------------------------------------------------------------------
